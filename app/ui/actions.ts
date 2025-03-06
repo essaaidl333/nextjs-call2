61fe39134b4a4365539ca7dd0a7da75e6c78f401
essa333 <essaaidl77146@gmail.com>
2025-03-07 02:53:56 +0300

    upsc

--- app/ui/actions.ts
+++ app/ui/actions.ts
@@ -1,10 +1,10 @@
-// "use server";
-// import { signIn } from "@/auth";
+"use server";
+import { signIn } from "@/auth";
 
 
 
-// export async function sing_gogel() {
-//   await signIn("google", { redirectTo: "/dashboard" });
-// }
+export async function sing_gogel() {
+  await signIn("google", { redirectTo: "/dashboard" });
+}
 
 
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,11 +1,11 @@
 'use client';
-// import { sing_gogel } from './actions';
+import { sing_gogel } from './actions';
 export default function LoginForm() {
  
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="mt-6 text-center text-gray-500">تسجيل الدخول باستخدام Google</div>
-      <form action={"/dashbord"} className="mt-3 flex justify-center">
+      <form action={sing_gogel} className="mt-3 flex justify-center">
         <button
           type="submit"
           className="flex items-center space-x-2 bg-gray-100 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-lg shadow-md"
